fcd65f06fed9d7d7040b30016e7026eb0618be9d
consolelog <leopportunity5@gmail.com>
2024-12-18 17:35:06 +0900

    2024-12-18 :: update

--- api.py
+++ api.py
@@ -1,8 +1,9 @@
 import httpx
 import requests
+import socket
 import streamlit as st
 
-SERVER_URL = "http://localhost:8080/v1/api"
+SERVER_URL = "http://localhost:8080/api/v1"
 
 
 def send_message(message, role):
@@ -17,15 +18,33 @@ def print_stream_message(response):
             result += chunk
             message_box.markdown(result)
 
+def request_chatroom(room_user_id):
+    print(room_user_id)
+    url = SERVER_URL + "/chat/"
+    headers = { "Content-Type": "application/json;charset=UTF-8" }
+    body = { "roomUserId": room_user_id }
+    response = requests.post(url=url, headers=headers, data=body)
+    print(response)
+    print(response.status_code)
+    print(response.text)
+
+def ip():
+    return socket.gethostbyname(socket.getfqdn())
+
 def request_message(message, cb):
     url = SERVER_URL + "/chat/stream"
-    body = {"content": (None, message)}
-
-    with httpx.stream(method="POST", url=url, files=body) as r:
-        if r.status_code == 200:
-            cb(r)
-        else:
-            print(f"에러 발생: HTTP {r.status_code} - {r.text}")
+    body = {
+        "content": (None, message),
+        "userId": ip()
+    }
+    response = requests.post(url=url,  files=body)
+    result = response.json()
+    return result["msg"]
+    # with httpx.stream(method="POST", url=url, files=body) as r:
+    #     if r.status_code == 200:
+    #         cb(r)
+    #     else:
+    #         print(f"에러 발생: HTTP {r.status_code} - {r.text}")
 
 def request_create_vectorstore(file):
     url = SERVER_URL + "/chat/create_vectorstore"
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-import streamlit as st
+import time
 
+import streamlit as st
+import socket
 import requests
-from api import send_message, print_stream_message, request_message
+from api import send_message, print_stream_message, request_message, request_chatroom
 from login import login
 
 st.set_page_config(
@@ -13,24 +15,45 @@ style = """
 <style>
 @import url(https://cdn.jsdelivr.net/gh/moonspam/NanumSquare@2.0/nanumsquare.css);
 html, body, [class*="css"]  {
-			font-family: 'NanumSquare', 'sans-serif';
+            font-family: 'NanumSquare', 'sans-serif';
 }
 </style>
 """
 st.markdown(style, unsafe_allow_html=True)
 
-requests.get(
-    url="http://localhost:8080/v1/api/"
-)
 
 with st.sidebar:
     if st.button("로그인"):
         login()
 
-message = st.chat_input("askanything")
+
+message = st.chat_input("App Page")
+SERVER_URL = "http://localhost:8080/api/v1"
+
+r = requests.get(SERVER_URL + "/chat/")
+print(r.text)
 
 if message:
     send_message(message, "human")
 
-    request_message(message, print_stream_message)
+    res = request_message(message, None)
 
+    # url = SERVER_URL + "/chat/ai"
+    # headers = {
+    #     "Content-Type": "application/json;charset=UTF-8",
+    #     "credential": "include"
+    # }
+    # body = {
+    #     "content": res,
+    #     "user_id": ""
+    # }
+    #
+    # requests.post(url=url, headers=headers, data=body)
+
+    def stream_data():
+        for word in res.split(" "):
+            yield word + " "
+            time.sleep(0.02)
+
+    with st.chat_message("ai"):
+        st.write_stream(stream_data)
